377741594ab876b63df986ea93e7e1fec1216636
J. Lima <0x133f@gmail.com>
2024-09-03 21:10:58 -0300

    Update aua_02.java

--- 29-08-24/aua_02.java
+++ 29-08-24/aua_02.java
@@ -1,11 +1,30 @@
 package aula2;
 import java.util.Scanner;
 
-public class Class
-public static void main(String[] args) {
-	int x, y, z;
-s
-	x = input.nextInt();
-	System.out.println("Digite o valor de X: ");
-	
+public class Aula2 {
+
+    public static void main(String[] args) {
+        int x, y, z;
+
+        Scanner stdin = new Scanner(System.in);
+
+        System.out.print("Type X value = ");
+        x = stdin.nextInt();
+
+        System.out.print("Type Y value = ");
+        y = stdin.nextInt();
+
+        System.out.print("Type Z value = ");
+        z = stdin.nextInt();
+
+        if ((x == y) && (y == z)) {
+            System.out.println("All are equal (X, Y, Z)");
+        } else if ((x > y) && (x > z)) {
+            System.out.print("Higher Value (X) ==> " + x);
+        } else if (y > z) {
+            System.out.print("Higher Value (Y) ==> " + y);
+        } else {
+            System.out.print("Higher Value (Z) ==> " + z);
+        }
+    }
 }
